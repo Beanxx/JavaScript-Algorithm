6548712073316225a0a34154791603f99dd8196e
Beanxx <tnqls991211@naver.com>
2022-06-24 18:08:13 +0900

    feat: solving Lv.1_2016년.js

---
+++ Programmers/Lv. 1/2016년.js
@@ -0,0 +1,38 @@
+// 2022.06.24(Fri)
+// 2016년.js
+
+function solution(a, b) {
+  const days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+  const date = new Date(`2016, ${a}, ${b}`);
+  return days[date.getDay()];
+}
+
+// new Date(year, monthIndex, day)
+// getDay(): 주어진 날짜의 현지 시간 기준 요일을 반환 (0: 일요일)
+// days[~~.getDay()]: getDay()로 요일 해당하는 index 번호를 리턴 (0: 일, 1: 월, ...)
+// -> days 배열에서 해당하는 요일 문자열 리턴
+
+// ---------
+
+// other way
+// Date() 메서드 사용하지 않고 푸는 방법
+function solution(a, b) {
+  const days = ["THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"];
+  const months = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+  // 1/1일에서  -> a/b일까지 며칠 차이인지 저장
+  let count = 0;
+
+  // a월 전까지 모든 달에 대해 각 달의 날짜 수를 더해줌
+  for (let i = 0; i < a - 1; i++) {
+    count += months[i];
+  }
+
+  // b일을 더해주고 1/0일이 아닌 1/1일부터 시작하므로 -1
+  count += b;
+
+  return days[count % 7];
+}
+
+// Date() 메서드를 활용하지 않는 방법도 이해하려고 했는데 로직은 어느정도 이해가는데
+// 왜 'THU'부터 시작하는지, 왜 a-1를 해주는지 확실하게 완전 이해하진 못했다..
