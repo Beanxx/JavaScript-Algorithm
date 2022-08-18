4005626623fe12533a86717e6ebb226cc32bb868
Beanxx <tnqls991211@naver.com>
2022-08-18 22:51:26 +0900

    feat: [Baekjoon] 18108_1998년생인 내가 태국에서는 2541년생?!.js

---
+++ Baekjoon/18108.js
@@ -0,0 +1,8 @@
+// 18108 1998년생인 내가 태국에서는 2541년생?!
+// 2022.08.18(목)
+
+const input = require("fs").readFileSync("input.txt").toString().split(" ");
+
+const num = Number(input) - 543;
+
+console.log(num);
