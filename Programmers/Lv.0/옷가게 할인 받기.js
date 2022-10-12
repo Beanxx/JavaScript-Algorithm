03b6d9487dc9f181ca24d33c9e361eef4a5948e7
Beanxx <tnqls991211@naver.com>
2022-10-12 19:35:08 +0900

    feat: [Programmers] Lv.0.옷가게 할인 받기.js

---
+++ Programmers/Lv.0/옷가게 할인 받기.js
@@ -0,0 +1,17 @@
+// 2022.10.12(Wed)
+
+function solution(price) {
+  let answer = 0;
+
+  if (price >= 500000) {
+    answer = Math.floor(price * 0.8);
+  } else if (price >= 300000) {
+    answer = Math.floor(price * 0.9);
+  } else if (price >= 100000) {
+    answer = Math.floor(price * 0.95);
+  } else {
+    answer = price;
+  }
+
+  return answer;
+}
