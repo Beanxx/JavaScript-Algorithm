914bdb6312e881e58ac3598dac5e6b5f443c6df2
Beanxx <tnqls991211@naver.com>
2022-10-17 21:26:20 +0900

    feat: [Programmers] Lv.0_점의 위치 구하기.js

---
+++ Programmers/Lv.0/점의 위치 구하기.js
@@ -0,0 +1,19 @@
+// 2022.10.17(Mon)
+
+// my way
+function solution(dot) {
+  if (dot[0] > 0 && dot[1] > 0) {
+    return 1;
+  } else if (dot[0] < 0 && dot[1] > 0) {
+    return 2;
+  } else if (dot[0] < 0 && dot[1] < 0) {
+    return 3;
+  } else {
+    return 4;
+  }
+}
+
+// other way
+function solution(dot) {
+  return dot[0] > 0 ? (dot[1] > 0 ? 1 : 4) : dot[1] > 0 ? 2 : 3;
+}
