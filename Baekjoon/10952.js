07b130dac0cdbb439355a03d4b61fc4095b2e0df
Beanxx <tnqls991211@naver.com>
2022-11-14 23:38:25 +0900

    [Baekjoon] 10952_A+B-5.js

---
+++ Baekjoon/10952.js
@@ -0,0 +1,12 @@
+// 10951 A+B - 5
+// 2022.11.14(Mon)
+
+const input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
+
+for (let i = 0; i < input.length; i++) {
+  let a = parseInt(input[i].split(" ")[0]);
+  let b = parseInt(input[i].split(" ")[1]);
+
+  if (a === 0 && b === 0) break;
+  else console.log(a + b);
+}
