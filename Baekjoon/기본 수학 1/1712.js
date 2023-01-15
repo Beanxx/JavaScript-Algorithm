db58aba73bd9a89388a92fda14c2458a60e6ad7a
Beanxx <tnqls991211@naver.com>
2023-01-15 23:51:46 +0900

    docs: Add comment 1712_손익분기점(기본수학1).js

--- Baekjoon/기본 수학 1/1712.js
+++ Baekjoon/기본 수학 1/1712.js
@@ -9,11 +9,12 @@ const input = require("fs")
   .split(" ")
   .map(Number);
 
-const A = input[0];
-const B = input[1];
-const C = input[2];
+const A = input[0]; // 고정비용
+const B = input[1]; // 가변비용
+const C = input[2]; // 판매비용
 
+// 고정비용에 플러스 비용인 가변비용이 판매비용보다 크거나 같은 경우 손익분기점이 존재할 수가 없다.
 if (B >= C) console.log(-1); // 손익분기점 존재하지 않는 경우
 else console.log(Math.floor(A / (C - B)) + 1);
 
-// 손익분기점: 총 수입 > 총비용(고정비용+가변비용)
+// 손익분기점: 총 수입(판매비용 = C) > 총비용(고정비용+가변비용 = A + B);
