6ad5c114a81f1a67cbb318e015c55a438cf4086c
Beanxx <tnqls991211@naver.com>
2023-01-29 00:38:18 +0900

    [Baekjoon] 1181_add comment

--- Baekjoon/정렬/1181.js
+++ Baekjoon/정렬/1181.js
@@ -13,6 +13,9 @@ input.shift(); // 맨 앞 입력값인 단어의 개수 N 제거
 
 const arr = [...new Set(input)]; // 중복 제거 후 배열로 변환
 
+// 길이순으로 먼저 정렬 후, 길이가 같으며 사전 순(내림차순)으로 정렬
 arr.sort((a, b) => a.length - b.length || a.localeCompare(b)); // 정렬
 
 console.log(arr.join("\n"));
+
+// localeCompare(): 인수로 지정된 문자열이 정렬상 string 객체의 문자열 뒤에 있으면 음수, 그 반대의 경우는 양수, 동등한 경우에는 0을 반환
