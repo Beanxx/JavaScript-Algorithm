0396d6aff9626c8575614e6b24f2492498268bd7
Beanxx <tnqls991211@naver.com>
2023-05-03 23:52:59 +0900

    [Programmers] Lv.0_출력, 연산 3문제 완료

---
+++ Programmers/Lv.0/홀짝 구분하기.js
@@ -0,0 +1,16 @@
+// 2023.05.03(Wed)
+
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on("line", function (line) {
+  input = line.split(" ");
+}).on("close", function () {
+  n = Number(input[0]);
+  console.log(`${n} is ${n % 2 === 0 ? "even" : "odd"}`);
+});
