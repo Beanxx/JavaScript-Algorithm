9f2d0512a6ac1d1758b2c0e7f16190766f1d786c
Beanxx <tnqls991211@naver.com>
2024-01-06 00:34:21 +0900

    [Programmers] add explain

--- Programmers/Lv.0/코딩 기초 트레이닝/배열의 길이에 따라 다른 연산하기.js
+++ Programmers/Lv.0/코딩 기초 트레이닝/배열의 길이에 따라 다른 연산하기.js
@@ -13,8 +13,12 @@ function solution(arr, n) {
 
 // other way
 function solution(arr, n) {
-  return arr.map((num, idx) => (arr.length % 2 !== idx % 2 ? num + n : num));
+  return arr.map((el, idx) => (arr.length % 2 !== idx % 2 ? el + n : el));
 }
+
+// arr.length 홀 -> 짝수 인덱스에 +n
+// arr.length 짝 -> 홀수 인덱스 +n
+// => arr.length % 2 !== idx % 2
 
 console.log(solution([49, 12, 100, 276, 33], 27)); // [76, 12, 127, 276, 60]
 console.log(solution([444, 555, 666, 777], 100)); // 	[444, 655, 666, 877]
--- Programmers/Lv.0/코딩 기초 트레이닝/홀수 vs 짝수.js
+++ Programmers/Lv.0/코딩 기초 트레이닝/홀수 vs 짝수.js
@@ -3,10 +3,11 @@
 // my way
 function solution(num_list) {
   let odd = num_list
-    .filter((el, i) => (i + 1) % 2 === 1)
+    .filter((_, i) => (i + 1) % 2 === 1)
     .reduce((acc, cur) => acc + cur); // 홀
+
   let even = num_list
-    .filter((el, i) => (i + 1) % 2 === 0)
+    .filter((_, i) => (i + 1) % 2 === 0)
     .reduce((acc, cur) => acc + cur); // 짝
 
   return Math.max(odd, even);
@@ -18,11 +19,13 @@ function solution(num_list) {
   let odd = 0;
 
   num_list.map((v, idx) => {
-    !(idx % 2) ? (even += v) : (odd += v);
+    !((idx + 1) % 2) ? (odd += v) : (even += v);
   });
 
-  return odd > even ? odd : even;
+  return Math.max(odd, even);
 }
+
+// !(idx % 2) => 값이 0이 아닌 경우 = 홀(odd)
 
 console.log(solution([4, 2, 6, 1, 7, 6])); // 17
 console.log(solution([-1, 2, 5, 6, 3])); // 8
