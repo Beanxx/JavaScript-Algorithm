3f4839aaffb67d01208b40bbf4d8ca00434cf35b
Beanxx <tnqls991211@naver.com>
2024-01-05 23:58:44 +0900

    [programmers] Lv.0_배열의 길이에 따른 다른 연산하기.js

---
+++ Programmers/Lv.0/코딩 기초 트레이닝/배열의 길이에 따라 다른 연산하기.js
@@ -0,0 +1,20 @@
+// 2024.01.05(Fri)
+
+// my way
+function solution(arr, n) {
+  let arrLength = arr.length;
+
+  if (arrLength % 2 === 1) {
+    return arr.map((el, i) => (i % 2 === 0 ? el + n : el));
+  } else {
+    return arr.map((el, i) => (i % 2 === 1 ? el + n : el));
+  }
+}
+
+// other way
+function solution(arr, n) {
+  return arr.map((num, idx) => (arr.length % 2 !== idx % 2 ? num + n : num));
+}
+
+console.log(solution([49, 12, 100, 276, 33], 27)); // [76, 12, 127, 276, 60]
+console.log(solution([444, 555, 666, 777], 100)); // 	[444, 655, 666, 877]
