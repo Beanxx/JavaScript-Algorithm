ee4dfa466bd56d499ba5ac6ebe2fd6895575bf37
Beanxx <tnqls991211@naver.com>
2024-01-06 21:10:59 +0900

    [programmers] Lv.0_공백으로 구분하기 1.js

---
+++ Programmers/Lv.0/코딩 기초 트레이닝/공백으로 구분하기 1.js
@@ -0,0 +1,9 @@
+// 2024.01.06(Sat)
+
+// my way
+function solution(my_string) {
+  return my_string.split(" ");
+}
+
+console.log(solution("i love you")); // ["i", "love", "you"]
+console.log(solution("programmers")); // ["programmers"]
