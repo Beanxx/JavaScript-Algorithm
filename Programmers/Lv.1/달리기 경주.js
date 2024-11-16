ee2e696e994c1ddec94df419e17a4ce2315c6d9c
Beanxx <tnqls991211@naver.com>
2024-11-16 16:08:24 +0900

    [Programmers] Lv.1_달리기 경주 (Refactor)

--- Programmers/Lv.1/달리기 경주.js
+++ Programmers/Lv.1/달리기 경주.js
@@ -2,13 +2,12 @@
 
 // my way
 function solution(players, callings) {
-  const playerIndices = {};
-  players.forEach((player, index) => {
-    playerIndices[player] = index;
-  });
+  const obj = Object.fromEntries(players.map((player, i) => [player, i]));
+
+  // Object.fromEntries([["mumu", 0], ["soe", 1]]); => { mumu: 0, soe: 1 }
 
   for (let el of callings) {
-    const idx = playerIndices[el];
+    const idx = obj[el];
 
     if (idx > 0) {
       const prevPlayer = players[idx - 1];
@@ -16,13 +15,30 @@ function solution(players, callings) {
       players[idx] = prevPlayer;
       players[idx - 1] = el;
 
-      playerIndices[el] = idx - 1;
-      playerIndices[prevPlayer] = idx;
+      obj[el] = idx - 1;
+      obj[prevPlayer] = idx;
     }
   }
 
   return players;
 }
+
+// function solution(players, callings) {
+//     for (let el of callings) {
+//         const idx = players.indexOf(el);
+
+//         if (players.indexOf(el)) {
+//             let temp = players[idx];
+//             players[idx] = players[idx-1];
+//             players[idx-1] = temp;
+//         }
+//     }
+
+//      return players;
+// }
+
+// 처음에 이렇게 풀었다가 시간 초과로 통과 못함..
+// indexOf()가 시간 효율을 많이 잡아먹는듯;-;
 
 console.log(
   solution(
